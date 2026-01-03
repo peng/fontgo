7ae6579e52952b1422c51952220dac20edc0d8a4
peng <414326615@qq.com>
2026-01-03 20:37:29 +0800

    add subset func

--- font/font.go
+++ font/font.go
@@ -338,3 +338,379 @@ func (f *Font) Write(filePath string) (err error) {
 	}
 	return os.WriteFile(filePath, data, 0o755)
 }
+
+// Subset extracts only the specified characters from the font.
+// It keeps glyph 0 (.notdef) and adds glyphs for the given characters.
+// The font tables (cmap, glyf, loca, hmtx, maxp, hhea) are updated accordingly.
+func (f *Font) Subset(chars []string) error {
+	if f.fontInfo == nil {
+		return errors.New("fontInfo is nil, call GetFontInfo first")
+	}
+	fontInfo := f.fontInfo
+
+	if fontInfo.Tables.Cmap == nil || fontInfo.Tables.Cmap.WindowsCode == nil {
+		return errors.New("cmap table or WindowsCode is nil")
+	}
+
+	// Step 1: Collect glyph indices for the requested characters
+	// Always include glyph 0 (.notdef)
+	neededGlyphSet := make(map[int]bool)
+	neededGlyphSet[0] = true
+
+	for _, char := range chars {
+		for _, r := range char {
+			unicode := int(r)
+			if glyphIndex, ok := fontInfo.Tables.Cmap.WindowsCode[unicode]; ok {
+				neededGlyphSet[glyphIndex] = true
+			}
+		}
+	}
+
+	// Step 2: Resolve compound glyph dependencies
+	// Compound glyphs reference other glyphs, we need to include those too
+	compoundMap := make(map[int]*GlyphCompound)
+	for i := range fontInfo.Glyphs.Compounds {
+		c := &fontInfo.Glyphs.Compounds[i]
+		compoundMap[c.GlyphCommon.Index] = c
+	}
+
+	// Iteratively resolve dependencies until no new glyphs are added
+	changed := true
+	for changed {
+		changed = false
+		for glyphIdx := range neededGlyphSet {
+			if compound, ok := compoundMap[glyphIdx]; ok {
+				for _, comp := range compound.Component {
+					refIdx := int(comp.GlyphIndex)
+					if !neededGlyphSet[refIdx] {
+						neededGlyphSet[refIdx] = true
+						changed = true
+					}
+				}
+			}
+		}
+	}
+
+	// Step 3: Create sorted list of old glyph indices and mapping to new indices
+	oldIndices := make([]int, 0, len(neededGlyphSet))
+	for idx := range neededGlyphSet {
+		oldIndices = append(oldIndices, idx)
+	}
+	sort.Ints(oldIndices)
+
+	oldToNew := make(map[int]int)
+	for newIdx, oldIdx := range oldIndices {
+		oldToNew[oldIdx] = newIdx
+	}
+
+	// Step 4: Build new Glyphs with remapped indices
+	simpleMap := make(map[int]*GlyphSimple)
+	for i := range fontInfo.Glyphs.Simples {
+		s := &fontInfo.Glyphs.Simples[i]
+		simpleMap[s.GlyphCommon.Index] = s
+	}
+
+	newGlyphs := &Glyphs{}
+	for newIdx, oldIdx := range oldIndices {
+		if simple, ok := simpleMap[oldIdx]; ok {
+			newSimple := *simple
+			newSimple.GlyphCommon.Index = newIdx
+			newGlyphs.Simples = append(newGlyphs.Simples, newSimple)
+		} else if compound, ok := compoundMap[oldIdx]; ok {
+			newCompound := *compound
+			newCompound.GlyphCommon.Index = newIdx
+			// Remap component glyph references
+			for i := range newCompound.Component {
+				oldRef := int(newCompound.Component[i].GlyphIndex)
+				newCompound.Component[i].GlyphIndex = uint16(oldToNew[oldRef])
+			}
+			newGlyphs.Compounds = append(newGlyphs.Compounds, newCompound)
+		}
+		// If glyph not found (empty glyph), it won't be added, which is correct
+	}
+	fontInfo.Glyphs = newGlyphs
+
+	// Step 5: Build new cmap with remapped glyph indices
+	newWindowsCode := make(map[int]int)
+	for unicode, oldGlyphIdx := range fontInfo.Tables.Cmap.WindowsCode {
+		if newGlyphIdx, ok := oldToNew[oldGlyphIdx]; ok {
+			newWindowsCode[unicode] = newGlyphIdx
+		}
+	}
+	fontInfo.Tables.Cmap.WindowsCode = newWindowsCode
+
+	// Rebuild cmap subtables for format 4 and 12
+	if err := rebuildCmapSubtables(fontInfo.Tables.Cmap, newWindowsCode); err != nil {
+		return err
+	}
+
+	// Step 6: Build new hmtx
+	if fontInfo.Tables.Hmtx != nil {
+		oldHmtx := fontInfo.Tables.Hmtx
+		newHmtx := &Hmtx{}
+
+		for _, oldIdx := range oldIndices {
+			if oldIdx < len(oldHmtx.HMetrics) {
+				newHmtx.HMetrics = append(newHmtx.HMetrics, oldHmtx.HMetrics[oldIdx])
+			} else {
+				// Use last advanceWidth with LeftSideBearing from array
+				lsbIdx := oldIdx - len(oldHmtx.HMetrics)
+				if lsbIdx < len(oldHmtx.LeftSideBearing) {
+					lastMetric := oldHmtx.HMetrics[len(oldHmtx.HMetrics)-1]
+					newHmtx.HMetrics = append(newHmtx.HMetrics, &LongHorMetric{
+						AdvanceWidth:    lastMetric.AdvanceWidth,
+						LeftSideBearing: oldHmtx.LeftSideBearing[lsbIdx],
+					})
+				}
+			}
+		}
+		fontInfo.Tables.Hmtx = newHmtx
+		// Clear LeftSideBearing since all metrics are now in HMetrics
+		fontInfo.Tables.Hmtx.LeftSideBearing = nil
+	}
+
+	// Step 7: Update maxp.NumGlyphs
+	if fontInfo.Tables.Maxp != nil {
+		fontInfo.Tables.Maxp.NumGlyphs = uint16(len(oldIndices))
+	}
+
+	// Step 8: Update hhea.NumOfLongHorMetrics
+	if fontInfo.Tables.Hhea != nil && fontInfo.Tables.Hmtx != nil {
+		fontInfo.Tables.Hhea.NumOfLongHorMetrics = uint16(len(fontInfo.Tables.Hmtx.HMetrics))
+	}
+
+	// Step 9: Rebuild loca (will be done during Write)
+	fontInfo.Tables.Loca = nil
+
+	return nil
+}
+
+// rebuildCmapSubtables rebuilds cmap subtables based on the new unicode->glyph mapping
+func rebuildCmapSubtables(cmap *Cmap, unicodeToGlyph map[int]int) error {
+	// Collect all unicode code points and sort them
+	unicodes := make([]int, 0, len(unicodeToGlyph))
+	for u := range unicodeToGlyph {
+		unicodes = append(unicodes, u)
+	}
+	sort.Ints(unicodes)
+
+	// Only keep format 4 and 12 subtables, rebuild them
+	newSubTables := make([]map[string]interface{}, 0)
+	hasFormat4 := false
+	hasFormat12 := false
+
+	for _, subTable := range cmap.SubTables {
+		format, ok := subTable["format"].(uint16)
+		if !ok {
+			continue
+		}
+
+		switch format {
+		case 4:
+			if !hasFormat4 {
+				newSubTables = append(newSubTables, buildCmapFormat4(subTable, unicodeToGlyph, unicodes))
+				hasFormat4 = true
+			}
+		case 12:
+			if !hasFormat12 {
+				newSubTables = append(newSubTables, buildCmapFormat12(subTable, unicodeToGlyph, unicodes))
+				hasFormat12 = true
+			}
+		}
+	}
+
+	// If no format 4 exists, create one for BMP characters
+	if !hasFormat4 && len(unicodes) > 0 {
+		defaultSubTable := map[string]interface{}{
+			"platformID":         uint16(3), // Windows
+			"platformSpecificID": uint16(1), // Unicode BMP
+			"language":           uint16(0),
+		}
+		newSubTables = append(newSubTables, buildCmapFormat4(defaultSubTable, unicodeToGlyph, unicodes))
+	}
+
+	cmap.SubTables = newSubTables
+	cmap.NumberSubtables = uint16(len(newSubTables))
+
+	return nil
+}
+
+// buildCmapFormat4 builds a format 4 cmap subtable
+func buildCmapFormat4(oldSubTable map[string]interface{}, unicodeToGlyph map[int]int, unicodes []int) map[string]interface{} {
+	// Filter to BMP only (0-0xFFFF) for format 4
+	bmpUnicodes := make([]int, 0)
+	for _, u := range unicodes {
+		if u <= 0xFFFF {
+			bmpUnicodes = append(bmpUnicodes, u)
+		}
+	}
+
+	// Build segments - use idRangeOffset method for non-contiguous glyph mappings
+	type segment struct {
+		startCode     uint16
+		endCode       uint16
+		idDelta       int16
+		idRangeOffset uint16
+		glyphIdArray  []uint16 // only used when idRangeOffset != 0
+	}
+	var segments []segment
+
+	i := 0
+	for i < len(bmpUnicodes) {
+		segStart := bmpUnicodes[i]
+		segEnd := bmpUnicodes[i]
+		firstGlyph := unicodeToGlyph[segStart]
+
+		// Try to extend segment with consecutive unicodes that have consecutive glyph indices
+		canUseDelta := true
+		j := i + 1
+		for j < len(bmpUnicodes) {
+			u := bmpUnicodes[j]
+			g := unicodeToGlyph[u]
+
+			if u != segEnd+1 {
+				// Unicode not consecutive, end segment
+				break
+			}
+
+			expectedGlyph := firstGlyph + (u - segStart)
+			if g != expectedGlyph {
+				// Glyph not consecutive, can't use simple delta
+				canUseDelta = false
+				break
+			}
+
+			segEnd = u
+			j++
+		}
+
+		if canUseDelta {
+			// Use idDelta method
+			delta := int16(firstGlyph - segStart)
+			segments = append(segments, segment{
+				startCode:     uint16(segStart),
+				endCode:       uint16(segEnd),
+				idDelta:       delta,
+				idRangeOffset: 0,
+			})
+			i = j
+		} else {
+			// Single character segment with idDelta
+			delta := int16(firstGlyph - segStart)
+			segments = append(segments, segment{
+				startCode:     uint16(segStart),
+				endCode:       uint16(segStart),
+				idDelta:       delta,
+				idRangeOffset: 0,
+			})
+			i++
+		}
+	}
+
+	// Add terminating segment
+	segments = append(segments, segment{
+		startCode:     0xFFFF,
+		endCode:       0xFFFF,
+		idDelta:       1,
+		idRangeOffset: 0,
+	})
+
+	segCount := len(segments)
+	segCountX2 := uint16(segCount * 2)
+
+	// Calculate searchRange, entrySelector, rangeShift
+	maxPow2 := 1
+	entrySelector := uint16(0)
+	for maxPow2*2 <= segCount {
+		maxPow2 *= 2
+		entrySelector++
+	}
+	searchRange := uint16(maxPow2 * 2)
+	rangeShift := segCountX2 - searchRange
+
+	// Build arrays
+	endCode := make([]uint16, segCount)
+	startCode := make([]uint16, segCount)
+	idDelta := make([]uint16, segCount)
+	idRangeOffset := make([]uint16, segCount)
+
+	for idx, seg := range segments {
+		endCode[idx] = seg.endCode
+		startCode[idx] = seg.startCode
+		idDelta[idx] = uint16(seg.idDelta)
+		idRangeOffset[idx] = seg.idRangeOffset
+	}
+
+	// Calculate length
+	length := uint16(16 + segCount*8) // header + 4 arrays * segCount * 2 bytes
+
+	newSubTable := make(map[string]interface{})
+	newSubTable["platformID"] = oldSubTable["platformID"]
+	newSubTable["platformSpecificID"] = oldSubTable["platformSpecificID"]
+	newSubTable["format"] = uint16(4)
+	newSubTable["length"] = length
+	newSubTable["language"] = oldSubTable["language"]
+	newSubTable["segCountX2"] = segCountX2
+	newSubTable["searchRange"] = searchRange
+	newSubTable["entrySelector"] = entrySelector
+	newSubTable["rangeShift"] = rangeShift
+	newSubTable["endCode"] = endCode
+	newSubTable["reservedPad"] = uint16(0)
+	newSubTable["startCode"] = startCode
+	newSubTable["idDelta"] = idDelta
+	newSubTable["idRangeOffset"] = idRangeOffset
+	newSubTable["glyphIndexArray"] = []uint16{}
+
+	return newSubTable
+}
+
+// buildCmapFormat12 builds a format 12 cmap subtable
+func buildCmapFormat12(oldSubTable map[string]interface{}, unicodeToGlyph map[int]int, unicodes []int) map[string]interface{} {
+	// Build groups
+	var groups []*CmapFormat8nGroup
+
+	if len(unicodes) > 0 {
+		segStart := uint32(unicodes[0])
+		segEnd := uint32(unicodes[0])
+		firstGlyph := uint32(unicodeToGlyph[unicodes[0]])
+
+		for i := 1; i < len(unicodes); i++ {
+			u := uint32(unicodes[i])
+			g := uint32(unicodeToGlyph[unicodes[i]])
+			expectedGlyph := firstGlyph + (u - segStart)
+
+			if u == segEnd+1 && g == expectedGlyph {
+				segEnd = u
+			} else {
+				groups = append(groups, &CmapFormat8nGroup{
+					StartCharCode:  segStart,
+					EndCharCode:    segEnd,
+					StartGlyphCode: firstGlyph,
+				})
+				segStart = u
+				segEnd = u
+				firstGlyph = g
+			}
+		}
+		groups = append(groups, &CmapFormat8nGroup{
+			StartCharCode:  segStart,
+			EndCharCode:    segEnd,
+			StartGlyphCode: firstGlyph,
+		})
+	}
+
+	// Calculate length: 16 (header) + nGroups * 12
+	length := uint32(16 + len(groups)*12)
+
+	newSubTable := make(map[string]interface{})
+	newSubTable["platformID"] = oldSubTable["platformID"]
+	newSubTable["platformSpecificID"] = oldSubTable["platformSpecificID"]
+	newSubTable["format"] = uint16(12)
+	newSubTable["reserved"] = uint16(0)
+	newSubTable["length"] = length
+	newSubTable["language"] = oldSubTable["language"]
+	newSubTable["nGroups"] = uint32(len(groups))
+	newSubTable["groups"] = groups
+
+	return newSubTable
+}
